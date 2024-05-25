40055131dea90ebb1698ad96ff4cad58ca5f239b
imriyakri <109199646+imriyakri@users.noreply.github.com>
2024-05-26 03:56:54 +0530

    Update mortgage_calculator.py

--- mortgage_calculator.py
+++ mortgage_calculator.py
@@ -95,7 +95,7 @@ def predict_demand():
     tipamount = st.number_input("Tip Amount", 0.0)
     
     # Read the CSV file containing pickup data
-    pickup_data = pd.read_csv('pickups_df.csv')
+    pickup_data = pd.read_csv('pickup.csv')
     
     # Convert month to numeric value
     month_dict = {"January": 1, "February": 2, "March": 3, "April": 4, "May": 5, "June": 6, "July": 7, "August": 8, "September": 9, "October": 10, "November": 11, "December": 12}
